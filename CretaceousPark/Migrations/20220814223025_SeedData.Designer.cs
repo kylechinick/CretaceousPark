e11dbea9597095fb53d99ef416d299561ff7cd90
Kyle Chinick <kylechinick@icloud.com>
2022-08-14 15:31:56 -0700

    Add seed data for dev testing

--- CretaceousPark/Migrations/20220814223025_SeedData.Designer.cs
+++ CretaceousPark/Migrations/20220814223025_SeedData.Designer.cs
@@ -2,6 +2,7 @@
 using CretaceousPark.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CretaceousPark.Migrations
 {
     [DbContext(typeof(CretaceousParkContext))]
-    partial class CretaceousParkContextModelSnapshot : ModelSnapshot
+    [Migration("20220814223025_SeedData")]
+    partial class SeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
